2afd6672d19438388ad299b094d7416f05e688f9
toleenJ <toleen.780@icloud.com>
2024-10-10 19:57:21 +0300

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,6 +1,1 @@
-git clone <repository-url>
-cd <repository-directory>
-git checkout -b update-linear-search
-git add <LinearSerchXGit>.java\
-git commit -m "Updated LinearSearch.java with comments,Toleen AL huthali, 445000362"
 
